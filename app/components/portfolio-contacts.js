9b565cd8838a83dfacbf7a2011f3fc0554d3b0df
Geng <snggeng@gmailcom>
2016-08-06 17:19:56 +0800

    resolved conflit

--- app/components/portfolio-contacts.js
+++ app/components/portfolio-contacts.js
@@ -11,7 +11,7 @@ export default Ember.Component.extend({
   },
   actions: {
     submit: function () {
-      alert('Thank you for getting in touch! I\'ll get back to you as soon as I can.')
+      alert('Form submit!')
     }
   }
 })
