aff713e034acbf4441c2f9a8827f1bda0b4a5157
Geng <snggeng@gmailcom>
2016-08-20 06:25:07 -0400

    testing ember routes and hbs on localhost

--- app/routes/resume.js
+++ app/routes/resume.js
@@ -1,4 +1,10 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
 export default Ember.Route.extend({
-});
+  model () {
+    return Ember.RSVP.hash({
+      profiles: this.store.findAll('profile'),
+      projects: this.store.findAll('project')
+    })
+  }
+})
